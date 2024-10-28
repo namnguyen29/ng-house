f9fb399eeb1e5fe7d3818d58b28158865f69a758
nnam <quocnam.dn29@gmail.com>
2024-10-28 12:05:10 +0700

    Learning: Day 21 - RxJS transformation

--- src/app/pages/fragments/fragments.component.ts
+++ src/app/pages/fragments/fragments.component.ts
@@ -1,9 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
-import { TabsComponent } from './components';
 
-import { defer, fromEventPattern, map, of, throwError } from 'rxjs';
+import {
+  delay,
+  fromEventPattern,
+  map,
+  merge,
+  of,
+  reduce,
+  scan,
+  throwError,
+  buffer,
+  interval,
+  fromEvent,
+  bufferTime
+} from 'rxjs';
 import { NodeEventHandler } from 'rxjs/internal/observable/fromEvent';
+
+import { TabsComponent } from './components';
 
 @Component({
   selector: 'app-fragments',
@@ -15,6 +29,22 @@ import { NodeEventHandler } from 'rxjs/internal/observable/fromEvent';
 export class FragmentsComponent implements OnInit {
   public counter = 0;
   public navigations: Array<string> = ['Profile', 'Dashboard', 'Setting', 'Contact'];
+  public users = [
+    {
+      id: 1,
+      firstName: 'Nam',
+      lastName: 'Nguyen',
+      email: 'nam@example.com',
+      amount: 4005
+    },
+    {
+      id: 2,
+      firstName: 'Tuấn',
+      lastName: 'Tran',
+      email: 'tuan@example.com',
+      amount: 4000
+    }
+  ];
 
   public ngOnInit(): void {
     const observer = {
@@ -35,9 +65,37 @@ export class FragmentsComponent implements OnInit {
     throwError(() => new Error('catch my error'));
 
     // run when observable has new subscribe
-    const random$ = defer(() => of(Math.random()));
-    random$.subscribe(observer);
-    random$.subscribe(observer);
-    random$.subscribe(observer);
+    //const random$ = defer(() => of(Math.random()));
+    // random$.subscribe(observer);
+    // random$.subscribe(observer);
+    // random$.subscribe(observer);
+
+    merge(of(this.users[0]).pipe(delay(2000)), of(this.users[1]).pipe(delay(4000))).pipe(
+      map((user) => ({
+        ...user,
+        fullName: `${user.firstName} ${user.lastName}`
+      }))
+    );
+
+    const queryParams$ = of({ param: 'uwu' });
+    queryParams$.pipe(map((x) => x.param));
+
+    // reduce, same with array reduce, emit value when all observable emit value
+    merge(of(this.users[0]).pipe(delay(2000)), of(this.users[1]).pipe(delay(4000))).pipe(
+      reduce((acc, curr) => acc + curr?.amount, 0)
+    );
+
+    // buffer, store value until the closing notifier emit
+    const interval$ = interval(1000);
+    const click$ = fromEvent(document, 'click');
+    interval$.pipe(buffer(click$));
+
+    // bufferTime, emit buffer value after x seconds
+    interval$.pipe(bufferTime(2000));
+
+    // scan vs reduce
+    merge(of(this.users[0]).pipe(delay(2000)), of(this.users[1]).pipe(delay(4000)))
+      .pipe(scan((acc, curr) => acc + curr?.amount, 0))
+      .subscribe(observer);
   }
 }
